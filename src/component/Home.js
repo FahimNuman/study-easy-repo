9931a25e8510820887f8a031e9358f56149bb98a
Md.Abdullah Al Numan Fahim <fahiomnuman87@gmail.com>
2022-10-11 09:44:51 +0600

    added header

---
+++ src/component/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const Home = () => {
+    return (
+        <section>
+            <div className="bg-gray-100">
+                <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-gray-900">
+                    <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-gray-800">
+                        Welcome To Study Easy
+                    </h1>
+                    <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl text-gray-900">
+                        Best E-Learning platform for judge yourself high quality Smart MCQ
+                        Appliances at extremely Free Cost.
+                    </p>
+                    
+                </div>
+            </div>
+            
+        </section>
+    );
+};
+
+export default Home;
